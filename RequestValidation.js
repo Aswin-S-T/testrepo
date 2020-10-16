c78fc45a8607ed2c7bb525377dd40aa3dd611d0f
ajith-er <ajith.er@alcodex.com>
2020-10-16 15:05:46 +0530

    SSO implementation

--- RequestValidation.js
+++ RequestValidation.js
@@ -1,48 +1,46 @@
 
 var DatabaseHandlerModule = require('./DatabaseHandler.js');
-var dbInstance = new  DatabaseHandlerModule.DatabaseHandler();
+var dbInstance = new DatabaseHandlerModule.DatabaseHandler();
 
 var jwtService = require('./jwtService.js');
 const bcrypt = require('bcrypt');
 
-function RequestValidation()
-{
+function RequestValidation() {
 
-	this.isValidUser = async function(req, res, callBack)
-    { 	
+	this.isValidUser = async function (req, res, callBack) {
 		try {
-			let token;
-			if (!req.headers.authorization) {
-				callBack(null);
-			}
-			token = req.headers.authorization.split(" ");
-			const isValid = await jwtService.decodeJwt(token[1]);
-
-			if (isValid == 'Not Valid')
-			{
-				callBack(null);
-			} else {
+			console.log('here', req.session);
+			if (req.isAuthenticated()) {
 				callBack("success");
+			} else if (req.headers.authorization) {
+				console.log('here jwt');
+				const token = req.headers.authorization.split(" ");
+				const isValid = await jwtService.decodeJwt(token[1]);
+				if (isValid == 'Not Valid') {
+					callBack(null);
+				} else {
+					callBack("success");
+				}
+			}else{
+				callBack(null);
 			}
 		} catch (err) {
-            callBack(null);
-        }
-		
+			callBack(null);
+		}
+
 	}
-	
-	this.checkUser = function(ssoId,password,callBack)
-    {
+
+	this.checkUser = function (ssoId, password, callBack) {
 		var query = {};
 		query['userName'] = ssoId;
-		dbInstance.GetDocumentByName('users', query, function(err, result)
-		{
-			if(result) {
-				bcrypt.compare(password, result.password, async function(err, res) {
-					if(res === true) {
+		dbInstance.GetDocumentByName('users', query, function (err, result) {
+			if (result) {
+				bcrypt.compare(password, result.password, async function (err, res) {
+					if (res === true) {
 						const token = await jwtService.getJwt({
 							user_id: ssoId
 						});
-						callBack({"token": token});
+						callBack({ "token": token });
 					} else {
 						callBack(null);
 					}
@@ -51,10 +49,10 @@ function RequestValidation()
 				callBack(null);
 			}
 		});
-    }
+	}
 }
 // export the class
 module.exports =
- {
-    RequestValidation
- };
+{
+	RequestValidation
+};
