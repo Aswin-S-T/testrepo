b647ece375f79fe4dea4eb0292f6f1bbf56ed366
ajith-er <ajith.er@alcodex.com>
2020-10-28 12:39:32 +0530

    Device statistics data api update

--- src/controllers/DeviceController.ts
+++ src/controllers/DeviceController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 import { StatusCodes } from 'http-status-codes';
+import { Devices } from "../models/Devices";
 
 /**
  * Add new device
@@ -47,4 +48,67 @@ export const getDeviceDetails = (req: Request, res: Response) => {
  * @param
  */
 export const deleteDevice = (req: Request, res: Response) => {
+}
+
+/**
+ * Get device statistics
+ * @method getDeviceStatistics
+ * @param
+ */
+export const getDeviceStatistics = (req: Request, res: Response) => {
+    var now = new Date();
+    now.setMinutes(now.getMinutes() - 20); // timestamp
+    now = new Date(now);
+
+    const pipeline: any = [
+        { $match: { isDeleted: false } },
+        {
+            $group: {
+                _id: "$isDeleted",
+                devices_total: { $sum: 1 },
+                devices_enabled: {
+                    "$sum": {
+                        "$cond": [{ "$eq": ["$activated", true] }, 1, 0]
+                    }
+                },
+                devices_disabled: {
+                    "$sum": {
+                        "$cond": [{ "$eq": ["$activated", false] }, 1, 0]
+                    }
+                },
+                devices_online: {
+                    "$sum": {
+                        "$cond": [{ "$gte": ["$lastDataReceiveTime", now] }, 1, 0]
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                devices_total: 1,
+                devices_enabled: 1,
+                devices_disabled: 1,
+                devices_online: 1
+            }
+        },
+    ]
+    Devices.aggregate(pipeline, function (err: any, data: any) {
+        let statistics: any = {
+            devices_total: 0,
+            devices_enabled: 0,
+            devices_disabled: 0,
+            devices_online: 0
+        }
+        if (data && data[0]) {
+            statistics = {
+                ...data[0]
+            }
+            delete (statistics._id)
+        }
+        return res.status(StatusCodes.OK).json({
+            ssuccess: true,
+            message: "Successfully retrieved data",
+            device_statistics: statistics
+        });
+    })
 }
