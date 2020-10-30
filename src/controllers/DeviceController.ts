dcda5d254e20febf0edc2963067efb322120ec56
ajith-er <ajith.er@alcodex.com>
2020-10-30 09:50:31 +0530

    Device list fetch api updates

--- src/controllers/DeviceController.ts
+++ src/controllers/DeviceController.ts
@@ -1,3 +1,4 @@
+import { getPagination } from '@utils';
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 import { StatusCodes } from 'http-status-codes';
@@ -22,6 +23,77 @@ export const addDevice = (req: Request, res: Response) => {
  * @param
  */
 export const listDevice = (req: Request, res: Response) => {
+    let sort: any = { _id: -1 }
+    const queryParams: any = req.query;
+    const match: any = { $and: [] }
+
+    switch (queryParams.status) {
+        case 'enabled':
+            match['$and'].push({ isDeleted: false })
+            match['$and'].push({ activated: true })
+            break;
+        case 'disabled':
+            match['$and'].push({ isDeleted: false })
+            match['$and'].push({ activated: false })
+            break;
+        case 'deleted':
+            match['$and'].push({ isDeleted: true })
+            break;
+        case 'online':
+            var now = new Date();
+            now.setMinutes(now.getMinutes() - 10); // timestamp
+            match['$and'].push({ isDeleted: false })
+            match['$and'].push({ activated: true })
+            match['$and'].push({ lastDataReceiveTime: { $gte: new Date(now).getTime() } })
+            break;
+        default:
+            match['$and'].push({ isDeleted: false })
+            break;
+    }
+
+    if (queryParams.search && queryParams.search != '') {
+        match['$and'].push({
+            $or: [
+                { 'deviceId': { '$regex': queryParams.search, '$options': 'i' } },
+                { 'location.city': { '$regex': queryParams.search, '$options': 'i' } },
+                { 'location.landMark': { '$regex': queryParams.search, '$options': 'i' } }
+            ],
+        })
+    }
+
+    const dataSkip = parseInt(queryParams.skip) || 0;
+    const dataLimit = parseInt(queryParams.limit) || 25;
+    let pipeline = [
+        { $match: match },
+        {
+            '$facet': {
+                metadata: [{ $count: "total" }],
+                data: [{ $sort: sort }, { $skip: dataSkip }, { $limit: dataLimit }]
+            }
+        }
+    ]
+
+    Devices.aggregate(pipeline, async function (err: any, data: any) {
+        const response: any = {
+            success: true,
+            message: "Data successfull retrieved",
+            device_list: [],
+            pagination: await getPagination(0, dataSkip, dataLimit)
+        };
+        if (data[0]) {
+            if (data[0].metadata[0]) {
+                response.pagination = await getPagination(data[0].metadata[0].total, dataSkip, dataLimit)
+            }
+            response.device_list = data[0].data
+        }
+
+        if (!err) {
+            return res.status(200).json(response);
+        }
+        if (err) {
+            console.log(err);
+        }
+    })
 }
 
 /**
@@ -77,7 +149,7 @@ export const getDeviceStatistics = (req: Request, res: Response) => {
                 },
                 devices_online: {
                     "$sum": {
-                        "$cond": [{ "$gte": ["$lastDataReceiveTime", timeStamp] }, 1, 0]
+                        "$cond": [{ $and: [{ "$gte": ["$lastDataReceiveTime", timeStamp] }, { "$eq": ["$activated", true] }] }, 1, 0]
                     }
                 }
             }
