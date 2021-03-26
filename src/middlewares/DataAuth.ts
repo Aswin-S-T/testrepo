98d3a662046e2351b6fde5c32c74fb06b5cfb580
ajith-alcodex <ajith.er@alcodex.com>
2021-03-26 12:53:46 +0530

    header issue fix

--- src/middlewares/DataAuth.ts
+++ src/middlewares/DataAuth.ts
@@ -4,8 +4,8 @@ import { StatusCodes } from 'http-status-codes';
 // Middleware to verify if user is an admin
 export const dataAuth = () => {
     return async function (req: Request, res: Response, next: NextFunction) {
-        if (req.headers['es_api_key']) {
-            if (req.headers['es_api_key'] == process.env.ES_API_KEY) {
+        if (req.headers['es-api-key']) {
+            if (req.headers['es-api-key'] == process.env.ES_API_KEY) {
                 next();
             } else {
                 return res.status(StatusCodes.UNAUTHORIZED).json({
